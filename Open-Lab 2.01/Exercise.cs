f302bef277a5fb4b38ed6c9cb577b0ae1a6196d8
z18ista03 <z18ista03@Z00P1-08.SPSKNM>
2019-10-28 09:24:09 +0100

    Add project files.

---
+++ Open-Lab 2.01/Exercise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_02._01
+{
+    class Exercise
+    {
+        public void Create5Numbers()
+        {
+            var numbers = new[] { 1f, 1.1f, 1.11f, 1.111f, 1.1111f };
+            Console.WriteLine(numbers[1]);
+        }
+    }
+}
---
+++ Open-Lab 2.01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Open_Lab_02._01
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            var exercise = new Exercise();
+            exercise.Create5Numbers();
+
+
+        }
+    }
+}
